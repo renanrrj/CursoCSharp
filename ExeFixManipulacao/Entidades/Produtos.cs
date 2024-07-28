d71c9a184a7d0297ab751a150197f34ed1e4f22e
Renan Ramos <drenanrrj@icloud.com>
2024-07-28 11:37:14 -0300

    Finalização do capítulo de Manipulação de arquivos.

---
+++ ExeFixManipulacao/Entidades/Produtos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ExeFixManipulacao.Entidades
+{
+    internal class Produtos
+    {
+        public string nomE { get; set; }
+        public double precO { get; set; }
+        public int quantidadE { get; set; }
+
+        public Produtos(string name, double price, int quantity)
+        {
+            nomE = name;
+            precO = price;
+            quantidadE = quantity;
+        }
+
+        public double Total()
+        {
+            return precO * quantidadE;
+        }
+    }
+}
--- ExeFixManipulacao/Program.cs
+++ ExeFixManipulacao/Program.cs
@@ -1,10 +1,43 @@
-﻿namespace ExeFixManipulacao
+﻿using System.Globalization;
+using ExeFixManipulacao.Entidades;
+namespace ExeFixManipulacao
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string Caminho = @"c:\Testes";
+            string Origem = @"c:\Testes\ArqOrigem.csv";
+            string Destino = @"c:\Testes\out\summary.csv";
+
+            try {
+                string[] linhas = File.ReadAllLines(Origem);
+
+                Directory.CreateDirectory(@"C:\Testes\out"); // Criação da pasta "out"
+
+                using (StreamWriter sw = File.AppendText(Destino))
+                { 
+                    foreach (string linha in linhas)
+                    {
+
+                        string[] Campos = linha.Split(',');
+                        string Nome = Campos[0];
+                        double Preco = double.Parse(Campos[1], CultureInfo.InvariantCulture);
+                        int Quantidade = int.Parse(Campos[2]);
+
+                        Produtos prod = new Produtos(Nome, Preco, Quantidade);
+
+                        sw.WriteLine(prod.nomE + "," + prod.Total().ToString("F2", CultureInfo.InvariantCulture));
+                    }
+                }
+
+                Console.WriteLine("");
+                Console.WriteLine("PARABÉNS ENCERRADO COM SUCESSO");
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine("ATENÇÃO OCORREU UM ERRO!! -> " + ex.Message);
+            }
         }
     }
 }
